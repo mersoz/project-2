64cc58fd478467402d2fcdd4a1b6b63c82d514e7
Muge Ersoz <muge@ersoz.org>
2017-03-03 09:31:29 +0000

    Initialize Git

---
+++ server.js
@@ -0,0 +1,65 @@
+// Require packages
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const session = require('express-session');
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+const { port, env, dbURI, sessionSecret } = require('./config/environment');
+const errorHandler = require('./lib/errorHandler');
+const routes = require('./config/routes');
+const customResponses = require('./lib/customResponses');
+const authentication = require('./lib/authentication');
+
+// Create an express app
+const app = express();
+
+// Set up our template engine
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.use(expressLayouts);
+
+// Set up static files folder
+app.use(express.static(`${__dirname}/public`));
+
+// Connect to database
+mongoose.connect(dbURI);
+
+// Set up middleware
+if(env !== 'test') app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride((req) => {
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+
+    return method;
+  }
+}));
+
+// Set up sessions
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Set up flash messages after sessions
+app.use(flash());
+
+// Set up custom middleware
+app.use(customResponses); // after app and flash, before routes
+app.use(authentication);
+
+// Set up routes - just before error handler
+app.use(routes);
+
+// Set up error handler - last piece of middleware before listen
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Express is listening to port ${port}`));
