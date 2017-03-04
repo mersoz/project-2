671f79140b149c28a505d42169defcf7b42610f8
Muge Ersoz <muge@ersoz.org>
2017-03-04 01:21:16 +0000

    Updated User model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String },
+  name: { type: String },
+  username: { type: String, required: true },
   email: { type: String },
-  password: { type: String, required: true}
+  password: { type: String, required: true },
+  profilePic: { type: String },
+  playlists: { type: Array } //later playlistSchema
 });
 
 userSchema
