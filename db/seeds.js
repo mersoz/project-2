61b37503676901fd90d544929ab52ba98f46a862
Muge Ersoz <muge@ersoz.org>
2017-03-10 10:27:54 +0000

    debug attempt

--- db/seeds.js
+++ db/seeds.js
@@ -6,7 +6,18 @@ mongoose.connect(dbURI);
 
 const User = require('../models/user');
 
-User.collection.drop()
-.then(() => {
-  mongoose.connection.close();
-});
+User.collection.drop();
+
+User
+  .create([{
+    name: 'Muge Ersoz',
+    username: 'mersoz',
+    email: 'muge@ersoz.org',
+    password: 'm',
+    passwordConfirmation: 'm'
+  }])
+  .then((users) => {
+    console.log(`${users.length} users created`);
+  })
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.connection.close());
